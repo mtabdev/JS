bbb347572944d786a3fdc5036d2e7a2c7c058088
mtabdev <84842990+mtabdev@users.noreply.github.com>
2023-11-05 06:44:56 +0000

    video 4 done

--- code_01/01.js
+++ code_01/01.js
@@ -1,1 +1,11 @@
-console.log("Hello Worlds!");
+//console.log("Hello Worlds!");
+const acId=12345;
+//acId=22;
+let accName;
+var accEmail="test@google.co.in";
+accAny="Don't know";
+for (let index = 0; index < 5; index++) {
+    accEmail='let@email.com';
+}
+console.log(acId);
+console.table([acId,accName,accEmail,accAny]);
